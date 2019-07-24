04d35c5191b45039affc3c223bde433b2deab713
Jake Lundberg <grove.mountain.github@nym.hush.com>
2019-07-23 22:07:00 -0700

    master: already doing an interesting dance here, make it spicier

--- test_engineering.sh
+++ test_engineering.sh
@@ -14,13 +14,17 @@ pe "vault login -method=ldap -path=ldap-mo username=${USERNAME} password=${USER_
 
 green "Read out the dynamic DB credentials and store them as variables"
 echo
-green "Getting dynamic db credentials from Vault. There are more elegant ways of doing this, but this shows the process well"
-pe "vault read -format=json db-blog/creds/mother-engineering-full-1h | jq -r '.data | to_entries | .[] | .key + \"=\" + .value ' > temp_creds"
-pe ". temp_creds"
-# pe "eval $(vault read -format=json db-blog/creds/mother-engineering-full-1h | jq -r '.data | to_entries | .[] | .key + "=" + .value ')"
+green "Getting dynamic db credentials from Vault. There are more elegant ways of doing this, but OSX doesn't support process substitution in its version of Bash, so working around (e.g. source <(cmd))"
 
+# This would be a way better thing to do as it prevents needing to redirect to a file.   However,
+# apparently Apple doesn't want to support newer versions of Bash, so awkwardness ensues
+# pe ". <(vault read -format=json db-blog/creds/mother-engineering-full-1h | jq -r '.data | to_entries | .[] | .key + \"=\" + .value ')"
+
+pe "vault read -format=json db-blog/creds/mother-engineering-full-1h | jq -r '.data | .[\"PGUSER\"] = .username | .[\"PGPASSWORD\"] = .password | del(.username, .password) | to_entries | .[] | .key + \"=\" + .value ' > .temp_db_creds"
+
+pe ". .temp_db_creds && rm .temp_db_creds"
 green "By setting the postgres environment variables to the dynamic creds, we can run PSQL with those dynamic creds"
-pe "export PGUSER=$username PGPASSWORD=$password"
+pe "export PGUSER=${PGUSER} PGPASSWORD=${PGPASSWORD}"
 
 green "Turn off globbing for the database query in an environment variable"
 pe "set -o noglob"
